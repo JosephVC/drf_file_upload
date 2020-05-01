3609d9d78c08066bb3e61dbc5569ba17f113c58a
JosephVC <josephvcardenas@gmail.com>
2020-04-30 20:37:12 -0700

    added Django CORS

--- fileupload/fileupload/settings.py
+++ fileupload/fileupload/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'file_app'
+    'file_app', 
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# Allow CORS for all domains
+CORS_ORIGIN_ALLOW_ALL =  True
 
 ROOT_URLCONF = 'fileupload.urls'
 
